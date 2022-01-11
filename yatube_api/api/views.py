1d592d909575f1dc5be3a38aa002f670b1030e36
master-cim <master-cim@yandex.ru>
2022-01-11 15:36:20 +0300

    публикации могут делать только авторизованные

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from django.shortcuts import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
 
@@ -17,8 +17,7 @@ class PostViewSet(viewsets.ModelViewSet):
     """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [
-        # IsAuthenticated,
+    permission_classes = [IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly]
     pagination_class = LimitOffsetPagination
 
