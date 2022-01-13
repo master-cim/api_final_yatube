fe1b7d2453a084fb364de485ea1f75535265fa32
master_cim <master-cim@yandex.ru>
2022-01-13 16:41:30 +0300

    подписки сохраняются

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,26 +1,51 @@
 from rest_framework import serializers
 from posts.models import Post, Comment, Group, User, Follow
 from rest_framework.fields import CurrentUserDefault
+from rest_framework.validators import UniqueTogetherValidator
+from rest_framework.exceptions import APIException
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('username',)
         ref_name = 'ReadOnlyUsers'
 
 
 class FollowSerializer(serializers.ModelSerializer):
-    following = UserSerializer('username')
-    user = serializers.StringRelatedField(
-        read_only=True,
-        default=serializers.CurrentUserDefault()
-    )
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username')
+    user = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username',
+        default=serializers.CurrentUserDefault())
+
+    # def create(self, validated_data):
+    #     """
+    #     Создает и возвращает экземпляр «Follow» с учетом проверенных данных.
+    #     """
+    #     return Follow.objects.create(**validated_data)
 
     class Meta:
         model = Follow
-        fields = '__all__'
+        exclude = ('id',)
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following'],
+                message='Подписка на автора уже есть!'
+            )
+        ]
+
+    # def validate_following(self, data):
+    #     if data['user'] == data['following']:
+    #         raise serializers.ValidationError(
+    #             'Зачем подписываться на себя?'
+    #         )
+    #     return data
+
 
 
 class CommentSerializer(serializers.ModelSerializer):
